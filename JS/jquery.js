bd628421521d2d8c6e9ec62a71e2fd463ffc5fc4
daikalej <alejodeluxe007@gmail.com>
2021-05-20 21:02:01 -0500

    bienvenidos

---
+++ JS/jquery.js
@@ -0,0 +1,109 @@
+$('.titulo1').click(function(){
+    $(this).css('background','yellow')
+  })
+
+  $('.titulo01').click(function(){
+    $(this).css('background','yellow')
+  })
+
+  $('#parrafo1').css("background","yellow").css("color","black")//
+
+
+  $('.asn').click(function(){
+    $(this).css('background','yellow')
+  })
+
+  $('.asn').mouseover(function(){
+    $(this).css("background","rgb(180, 250, 51")
+    $(this).css("color","black")
+  })
+  
+  $('.asn').mouseout(function(){
+    $(this).css("background","gray")
+  })
+
+
+
+
+  $('.opcion01').mouseover(function(){
+    $(this).css('font-size','50px')
+  })
+  
+  $('.opcion01').mouseout(function(){
+    $(this).css('font-size','20px')
+  })
+
+  $('.opcion02').mouseover(function(){
+    $(this).css('font-size','50px')
+  })
+  
+  $('.opcion02').mouseout(function(){
+    $(this).css('font-size','20px')
+  })
+
+
+  $('.opcion03').mouseover(function(){
+    $(this).css('font-size','50px')
+  })
+  
+  $('.opcion03').mouseout(function(){
+    $(this).css('font-size','20px')
+  })
+
+
+  $('.opcion04').mouseover(function(){
+    $(this).css('font-size','50px')
+  })
+  
+  $('.opcion04').mouseout(function(){
+    $(this).css('font-size','20px')
+  })
+
+
+
+  $('.opcion05').mouseover(function(){
+    $(this).css('font-size','50px')
+  })
+  
+  $('.opcion05').mouseout(function(){
+    $(this).css('font-size','20px')
+  })
+
+
+  $('.opcion06').mouseover(function(){
+    $(this).css('font-size','50px')
+  })
+  
+  $('.opcion06').mouseout(function(){
+    $(this).css('font-size','20px')
+  })
+
+  $('.opcion07').mouseover(function(){
+    $(this).css('font-size','50px')
+  })
+  
+  $('.opcion07').mouseout(function(){
+    $(this).css('font-size','20px')
+  })
+
+  $(Document).ready(function(){
+    alert('Bienvenidos a esta pagina.')
+  })
+
+
+  $('.boton').click(function(){
+    $('.caja2').hide("slow")
+  })
+  
+  $('.boton').dblclick(function(){
+    $('.caja2').show("slow")
+  })
+  
+  $('.adiccion').mouseover(function(){
+    $(this).css("background","rgb(180, 250, 51")
+    $(this).css("color","black")
+  })
+  
+  $('.adiccion').mouseout(function(){
+    $(this).css("background","gray")
+  })
